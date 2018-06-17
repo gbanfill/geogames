06e11d1dfe4a547372cd088e87a586289674ec40
gbanfill <gbanfill@gmail.com>
2018-06-17 22:19:01 +0100

    Added a spinner countdown to make the countdown easier to understand.

--- GeoGames/ModelGameCountDown.xaml.cs
+++ GeoGames/ModelGameCountDown.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using GeoGames.ViewModel;
 using Xamarin.Forms;
 
@@ -13,6 +15,31 @@ namespace GeoGames
 			BindingContext = ViewModelLocator.GameStartingViewModel;
 			ViewModelLocator.GameStartingViewModel.Navigation = Navigation;
 
+            this.Appearing += Handle_Appearing;
+            this.Disappearing += Handle_Disappearing;
+            cancellation = new CancellationToken();
+        }
+
+        CancellationToken cancellation;
+
+        async void Handle_Appearing(object sender, EventArgs e)
+        {
+            await RotateElement(loadingImage, cancellation);
+        }
+
+        void Handle_Disappearing(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private async Task RotateElement(VisualElement element, CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await element.RotateTo(360, 800, Easing.Linear);
+                await element.RotateTo(0, 0); // reset to initial position
+            }
         }
     }
 }
